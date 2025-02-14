f27312dfd3ae8f0879d976aec1666235b2a4be95
bietiaop <1527109126@qq.com>
2025-02-14 15:59:21 +0800

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -3,8 +3,5 @@ import NextAuth from 'next-auth';
 import authConfig from './auth.config';
 
 export const { handlers, auth, signIn, signOut, unstable_update } = NextAuth({
-  session: {
-    strategy: 'database',
-  },
   ...authConfig,
 });
